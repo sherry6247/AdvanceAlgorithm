9c22b1cc76af0f123ce01d7ca26943cfc917168a
sherry6247 <576141501@qq.com>
2017-12-27 14:11:45 +0800

    get all feature done

--- test2.py
+++ test2.py
@@ -17,7 +17,7 @@ def getallsmalifill(path):
 
 
 # 获取.smali文件中的opcode
-def getOpcode(path, txtpath, api_txtpath):
+def getOpcode(path, txt_dict, api_dict):
     # with open(path, 'r', encoding='UTF-8') as f:
     f = open(path, encoding='UTF-8')
     contents = f.readlines()
@@ -28,11 +28,16 @@ def getOpcode(path, txtpath, api_txtpath):
     contents = list(map(str.strip, contents))
     l = []
     r = []
-    all_list = []
+    allop_list = []
+    allap_list = []
     for con in contents:
         s_opcode = con.split(' ')[0]
         l.append(s_opcode)
-    return mapInstruct(l, txtpath)
+        t_method = con.split(' ')[-1]
+        r.append(t_method)
+    allop_list.append(mapInstruct(l, txt_dict))
+    allap_list.append(mapAPI(r, api_dict))
+    return allop_list, allap_list
 
 
 # 读取opcode.txt文件,将其转换为字典
@@ -58,52 +63,63 @@ def readAPItxt(path):
 
 
 # 将opcode与MRGITPV一一对应
-def mapInstruct(list, txtpath):
+def mapInstruct(list, txt_dict):
     instruct_list = []
     for l in list:
-        instruct_dict = readOptxt(txtpath)
-        if l in instruct_dict.keys():
-            instruct_list.append(instruct_dict[l])
+        if l in txt_dict.keys():
+            instruct_list.append(txt_dict[l])
     return instruct_list
 
-
 # 将API与自定义字母一一对应
-def mapAPI(list, api_txtpath):
+def mapAPI(list, api_dict):
     method_list = []
     for m in list:
-        api_dict = readAPItxt(api_txtpath)
-        if m in api_dict.keys():
-            method_list.append(api_dict[m])
+        for k in api_dict.keys():
+            if k in m:
+                method_list.append(api_dict[k])
+    # print(method_list)
     return method_list
 
-
 # 得到每一个.apk文件包含的所有.smali文件中的操作码的简称的集合的列表
-def getOpcodelist(path, txtpath, api_txtpath):
+def getOpcodelist(path, txt_dict, api_dict):
     allfilespath = getallsmalifill(path)
     opcode_list = []
+    api_list = []
     for filepath in allfilespath:
-        op_lists = getOpcode(filepath, txtpath, api_txtpath)
+        op_lists, ap_lists = getOpcode(filepath, txt_dict, api_dict)
         for op_list in op_lists:
             opcode_list.extend(op_list)
-    return opcode_list
-
+        for ap_list in ap_lists:
+            api_list.extend(ap_list)
+    return opcode_list, api_list
 
 if __name__ == '__main__':
     opcode_txt_path = './opcode.txt'
     api_path = './API.txt'
     mal_rootpath = 'G:\\machinelearning\\train_mal'
     ben_rootpath = 'G:\\machinelearning\\train_ben'
+    api_dict = readAPItxt(api_path)
+    opcode_dict = readOptxt(opcode_txt_path)
+    print(api_dict)
     i = 0
     for mal_file in os.listdir(mal_rootpath):
+        all_list = []
         i = i + 1
         # if i >= 8:
         m_path = os.path.join(mal_rootpath, mal_file)
         print(m_path)
-        opcode_list = getOpcodelist(m_path, opcode_txt_path, api_path)
+        name = m_path.split('.')[-2].split('\\')[-1]
+        print(name)
+        opcode_list, mal_api_list = getOpcodelist(m_path, opcode_dict, api_dict)
         # print(opcode_list)
-        with open('./instruct_txt/mal_txt/mal_' + str(i) + '.txt', 'w') as f:
-            for op in opcode_list:
-                f.write(op)
+        # print(mal_api_list)
+        # with open('./feature_txt/mal_f/mal_f_' + str(i) + '.txt', 'w') as f:
+        #     for op in opcode_list:
+        #         f.write(op)
+        #     f.write("\n")
+        #     for mal in mal_api_list:
+        #         f.write(mal)
+
         allfile = []  # 已经得到所有的Malware文件下的.smali中七类操作码
         print(i)
 
